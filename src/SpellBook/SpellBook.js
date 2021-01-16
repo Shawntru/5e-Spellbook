4136fb6f3959545eee601ef79830a0a771b06a13
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-01-16 11:26:53 -0700

    SpellBook view gets all local spells and fetches data

--- src/SpellBook/SpellBook.js
+++ src/SpellBook/SpellBook.js
@@ -1,7 +1,26 @@
 import './SpellBook.scss';
 import React from 'react';
+import { useEffect, useState } from 'react';
+import * as API from '../APIcalls';
+import SpellCard from '../SpellCard/SpellCard';
+import SpellDetails from '../SpellDetails/SpellDetails';
 
 const SpellBook = () => {
+  const [spellBook, setSpellBook] = useState([]);
+
+  useEffect(() => {
+    let spellBookSpells = [];
+    // debugger;
+    Object.values(localStorage).forEach((spell) =>
+      API.fetchSpellDetails(spell).then((data) => {
+        spellBookSpells.push(data);
+        if (localStorage.length === spellBookSpells.length) {
+          setSpellBook(spellBookSpells);
+        }
+      })
+    );
+  }, []);
+
   return <h1>SpellBook</h1>;
 };
 
