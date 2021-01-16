fe0f61edcb4dc92ef834ad7db07a1e351a704460
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-01-16 11:07:54 -0700

    Create SpellBook view

--- src/App/App.js
+++ src/App/App.js
@@ -3,19 +3,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from '../Homepage/Homepage';
 import SpellSearch from '../SpellSearch/SpellSearch';
+import SpellBook from '../SpellBook/SpellBook';
 
 const App = () => {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route
-          exact
-          path="/spells/:pcClass"
-          render={() => {
-            return <SpellSearch />;
-          }}
-        />
+        <Route exact path="/spells/:pcClass" component={SpellSearch} />
+        <Route exact path="/spellbook" component={SpellBook} />
       </Switch>
     </div>
   );
---
+++ src/SpellBook/SpellBook.js
@@ -0,0 +1,8 @@
+import './SpellBook.scss';
+import React from 'react';
+
+const SpellBook = () => {
+  return <h1>SpellBook</h1>;
+};
+
+export default SpellBook;
