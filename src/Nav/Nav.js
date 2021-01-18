647807ccd41771777967451473cd459dc2bfcaea
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-01-17 22:00:55 -0700

    Add nav bar links and styling

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -5,12 +5,17 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <nav className="nav-bar-wrapper">
-      <Link to={'/'} className="nav-link">
-        Select a Class
-      </Link>
-      <Link className="nav-link" to={'/spellbook'}>
-        Your Spell Book
-      </Link>
+      <div className="nav-underline-wrapper">
+        <div className="gold-left"></div>
+        <Link to={'/'} className="nav-link">
+          <h3>Select a Class</h3>
+        </Link>
+        <h2 className="site-title">The Dusty Tome</h2>
+        <Link className="nav-link" to={'/spellbook'}>
+          <h3>Your Spell Book</h3>
+        </Link>
+        <div className="gold-right"></div>
+      </div>
     </nav>
   );
 };
